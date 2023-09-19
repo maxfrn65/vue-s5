a11c00885e571f8d7a33747631ff4e5c0a4669eb
Maxime Fourna <65597525+maxfrn65@users.noreply.github.com>
2023-09-19 14:20:39 +0200

    feat added exercice 1 with components and exercice 2 with v-for

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,44 @@
 <script setup>
-  const symfony = 18;
-  const integration = 20;
-  const reseau = 19;
+  import Button from "@/components/Button.vue";
+  import {computed, ref} from "vue";
+  const notes_array = [{'matiere': 'Symfony','notes': [18,12,14,9]}, {'matiere': 'Intégration','notes': [20]},{'matiere': 'Réseau','notes': [19]}];
+  const note1= 12;
+  const note2= 15;
+  const note3= 14;
+  const moyenne = computed(() => {
+    return (note1 + note2 + note3) / 3;
+  });
+  const title = "Welcome to Vuejs 3 - The Progressive JavaScript Framework";
+  const p = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.";
 </script>
 
 <template>
+  <h1>{{ title }}</h1>
+  <p>{{ p }}</p>
+  <hr>
+  <br>
+  <Button />
+  <br>
   <ul>
-    <li>Note en Symfony : {{ symfony }}</li>
-    <li>Note en intégration : {{ integration }}</li>
-    <li>Note en réseau : {{ reseau }}</li>
+    <li v-for="item in notes_array">Notes en {{ item.matiere }} :
+      <span v-for="(note, index) in item.notes" :key="index" :style="{color : note < 10 ? 'red' : 'black'}">
+        {{ note }} {{ index < item.notes.length - 1 ? ', ' : '' }}
+      </span><br>
+    </li>
+    <li>
+      Notes semestres : {{ note1 }}, {{ note2 }}, {{ note3 }}
+      <br>
+      Moyenne : {{ moyenne.toFixed(2) }}
+    </li>
   </ul>
 </template>
 
 <style scoped>
+  h1 {
+    font-weight: bold;
+  }
 
+  .red {
+    color: red;
+  }
 </style>
---
+++ src/components/Button.vue
@@ -0,0 +1,24 @@
+<script setup>
+</script>
+
+<template>
+  <a href="https://vuejs.org/guide/introduction.html" target="_blank">See Docs <i class="fa-solid fa-arrow-right"></i></a>
+</template>
+
+<style scoped>
+  a {
+    display: inline-flex;
+    align-items: center;
+    gap: 15px;
+    color: white;
+    text-decoration: none;
+    background-color: #42b983;
+    padding: 15px;
+    border-radius: 10px;
+    font-weight: bold;
+  }
+
+  a:hover {
+    background-color: #3daa7d;
+  }
+</style>
